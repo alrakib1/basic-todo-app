10945dda0120c4b3200082808f76e278a6fa3d53
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-06 20:22:52 +0600

    added separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import TodoItem from "./components/TodoItem";
+
 import { dummyData } from "./data/todos";
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -12,6 +13,9 @@ function App() {
     );
   };
 
+
+
+
   const addTodo = (title: string) => {
     setTodos((prevTodos) => [
       { id: prevTodos.length + 1, title, completed: false },
@@ -19,20 +23,18 @@ function App() {
     ]);
   };
 
+
+const deleteTodo = (id: number) => {
+
+}
+
+
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
         <AddTodoForm onSubmit={addTodo} />
-        <div className="space-y-2">
-          {todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              onCompletedChange={setTodoCompleted}
-            />
-          ))}
-        </div>
+       <TodoList todos={todos} onCompletedChange={setTodoCompleted} onDelete={deleteTodo}/>
       </div>
     </main>
   );

9094fac88b41229d7e5600084c93bf356f7b90c9
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-06 11:21:07 +0600

    first commit

---
+++ src/data/todos.ts
@@ -0,0 +1,20 @@
+import { Todo } from "../types/todo";
+
+
+export const dummyData:Todo[] =[
+    {
+        id: 1,
+        title: "Learn React 👌",
+        completed: false
+    },
+    {
+        id: 2,
+        title: "Learn TypeScript 🤔",
+        completed: false
+    },
+    {
+        id: 3,
+        title: "Learn TailwindCSS 🎨",
+        completed: false
+    }
+]
